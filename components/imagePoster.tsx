05af811bd28d9cb94c50a4b43da6a77411e0f5b2
icezatoo <icetoeylove@gmail.com>
2019-11-25 11:58:04 +0700

    feat : add movie popular

---
+++ components/imagePoster.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { imageEndPoint } from "constant"
+import styled from "styled-components"
+
+const ImageContent = styled.img`
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+  overflow: hidden;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+  border-width: 0px;
+  border-radius: 4px;
+`
+
+const ImagePoster = ({ alt, path }) => (
+  <ImageContent
+    srcSet={`${imageEndPoint}/w154${path} 154w,
+             ${imageEndPoint}/w185${path} 185w,
+             ${imageEndPoint}/w342${path} 342w,
+             ${imageEndPoint}/w500${path} 500w`}
+    src={`${imageEndPoint}/w500${path}`}
+    alt={alt}
+  />
+)
+
+export default ImagePoster
---
+++ components/loader.tsx
@@ -0,0 +1,44 @@
+import react from "react"
+import styled from "styled-components"
+
+interface ILoading {
+  width?: number
+  height?: number
+}
+
+const Loader = styled.div`
+  border: 5px solid #312c2c;
+  border-radius: 50%;
+  border-top: 5px solid #e5091d;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  left: 50%;
+  position: absolute;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+
+  /* Safari */
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+const Loading: React.FC<ILoading> = ({ width = 50, height = 50 }) => {
+  return <Loader width={width} height={height}></Loader>
+}
+
+export default Loading
